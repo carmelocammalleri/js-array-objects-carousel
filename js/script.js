2238c0f23aa30d8681aef804acbcf70599d4d9f3
Melozzo <carmecamma1995@gmail.com>
2023-09-22 16:04:26 +0200

    [edit]: for each to take images

--- js/script.js
+++ js/script.js
@@ -8,11 +8,27 @@ const thumbNail = document.querySelector('.thumb')
 
 // 3. creare array
 const listImages = [
-  'img/01.webp',
-  'img/02.webp',
-  'img/03.webp',
-  'img/04.webp',
-  'img/05.webp'
+  {
+    image: 'img/01.webp',
+    title: 'Marvel\'s Spiderman Miles Morale',
+    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+}, {
+    image: 'img/02.webp',
+    title: 'Ratchet & Clank: Rift Apart',
+    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+}, {
+    image: 'img/03.webp',
+    title: 'Fortnite',
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+}, {
+    image: 'img/04.webp',
+    title: 'Stray',
+    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+}, {
+    image: 'img/05.webp',
+    title: "Marvel's Avengers",
+    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+}
 ]
 
 //5. creare contatore
@@ -21,15 +37,16 @@ printImg ()
 
 //4. inserire img dentro html
 function printImg (){
-  for(let i=0; i< listImages.length; i++){
-    const image = listImages[i];
-  
+  listImages.forEach((wepb)=>{
+
     // 7.condizioni bonus
-    thumbNail.innerHTML += `<div class="thumb-img"><img src="${image}" class=" item-thumb"></div>`
+    thumbNail.innerHTML += `<div class="thumb-img"><img src="${wepb.image}" class=" item-thumb"></div>`
   
     //8. stampa codice
-    imageWrapper.innerHTML += `<img src="${image}" class= " item hide">`;
-  }
+    imageWrapper.innerHTML += `<img src="${wepb.image}" class= " item hide">`;
+  })
+    
+
 }
 
 //6.
@@ -59,5 +76,3 @@ btnUp.addEventListener('click', function(){
   itemHide[counter].classList.remove('hide');
 
 })
-
-const img = document.querySelector('img')
